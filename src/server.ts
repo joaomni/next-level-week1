7aa337a78cd0fb23c4db164f5a0e73815b496364
joaoqsl <joaojpmoreira25@gmail.com>
2020-06-03 19:54:47 -0300

    Database connection, migrates, seeds, routes

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,14 @@
 import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(routes)
 
-const users = [
-    "Maicon",
-    "João",
-    "Ulisses",
-    "Bruno"
-]
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
-
-    response.json(filteredUsers)
-})
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user)
-})
-
-app.post('/users', (request, response) => {
-    const { name, email} = request.body
-
-    const user = {
-        'name': name,
-        'email': email
-    }
-
-    return response.json(user)
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
